b6291d532859d9b3bd2a350996434f2f1a16e101
나평근 <skvudrms54@gmail.com>
2020-06-15 00:06:17 +0900

    Study express-jwt Update .eslintrc.js, app.js, and auth.js

--- express-jwt/app.js
+++ express-jwt/app.js
@@ -1,13 +1,26 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+
+/* =======================
+    LOAD THE DEPENDENCIES
+==========================*/
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+/* =======================
+    LOAD THE CONFIG
+==========================*/
+const config = require('./config');
+
+
 var app = express();
+
+
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -18,6 +31,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+// set the secret key variable for jwt
+app.set('jwt-secret', config.secret);
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
